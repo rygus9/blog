80e74f80301d592d90bde6dd19985e4bdd308f12
Cuzz <rygus9@naver.com>
2023-08-29 00:46:35 +0900

    style: font 추가 및 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,28 @@
 import "./globals.css";
 
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Black_Han_Sans, Noto_Sans_KR } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+import { classNames } from "./_common/utils/classNames";
+
+const notoSansKr = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: "--notoSansKr",
+});
+
+const blackHanSans = Black_Han_Sans({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--blackHan",
+});
+
+const arita = localFont({
+  src: "./fonts/Arita-dotum-Medium.woff",
+  display: "swap",
+  variable: "--arita",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,8 +31,16 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ko">
+      <body
+        className={classNames(
+          arita.variable,
+          notoSansKr.variable,
+          blackHanSans.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 };
