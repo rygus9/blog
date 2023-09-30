02acf0674a8b05973dc1566203e41a4c6000868b
Cuzz <rygus9@naver.com>
2023-09-30 14:50:09 +0900

    feat: record 제목에 slug 링크 추가

--- app/record/page.tsx
+++ app/record/page.tsx
@@ -1,4 +1,8 @@
-import { ArrowUturnLeftIcon, CalendarIcon } from "@heroicons/react/20/solid";
+import {
+  ArrowUturnLeftIcon,
+  CalendarIcon,
+  LinkIcon,
+} from "@heroicons/react/20/solid";
 import Link from "next/link";
 
 import { classNames } from "../_common/utils/classNames";
@@ -37,8 +41,19 @@ const Page = () => {
         <div className="divide-y divide-back-em">
           {records.map(({ content, createdAt, title }) => (
             <article className="py-8 first:pt-0">
-              <h2 className="text-[22px] text-txt-500 font-semibold">
-                {title}
+              <h2
+                className="text-[22px] text-txt-500 font-semibold scroll-mt-20"
+                id={createdAt}
+              >
+                <a
+                  href={`#${createdAt}`}
+                  className={classNames(
+                    "flex items-center justify-start gap-1",
+                    "hover:text-txt-300",
+                  )}
+                >
+                  {title} <LinkIcon className="w-5 h-5 text-txt-300" />
+                </a>
               </h2>
               <span className="inline-flex gap-1 items-center text-sm text-txt-300 align-middle mt-1">
                 <CalendarIcon className="w-4 h-4" />
