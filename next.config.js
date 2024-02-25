cf557b7b265b94827dff632c19c2832e0faa8a36
Cuzz <rygus9@naver.com>
2024-02-25 23:34:16 +0900

    feat: nextconfig 수정

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
 }
 
 module.exports = nextConfig
